3b697ff97a0b81fb0fb69fc6b026f8da2140d9c0
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2023-05-21 01:59:03 +0600

    vercel.json

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     const toyCollection=client.db('toyMarket').collection('toys');
-    const indexKey = { name: 1, sellerName: 1 };
-    const indexOptions = { multipleFinding: "webfinding" };
+    // const indexKey = { name: 1, sellerName: 1 };
+    // const indexOptions = { multipleFinding: "webfinding" };
     //search
     app.get('/search/:text',async(req,res)=>{
       const searchText=req.params.text;
@@ -93,7 +93,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-     client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
